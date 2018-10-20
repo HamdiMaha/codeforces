c145fe52a4c503277d6fb9b395c629b9e30f0776
HamdiMaha <39532685+HamdiMaha@users.noreply.github.com>
2018-10-20 22:11:36 +0100

    Create HQ9+.cpp

---
+++ HQ9+.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s; cin>>s;
+	int flag=0;
+	for(int i = 0 ; i<s.size() ; i++){
+		if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9'){
+			flag=1;
+			break;
+		}
+			
+	}
+	if(flag == 1)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	return 0;
+}
