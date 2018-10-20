243bfe466ef81801964e9568573161b8c50d3cd3
HamdiMaha <39532685+HamdiMaha@users.noreply.github.com>
2018-10-20 18:14:42 +0100

    Create A. New Year and Counting Cards.cpp

---
+++ A. New Year and Counting Cards.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+ 
+using namespace std;
+
+
+int  main (){
+	string s; cin >> s;
+	int count = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if ((s[i] =='a') || (s[i] =='e') || (s[i] =='i') || (s[i] =='o') || (s[i] =='u')) 
+			count++;
+		if((s[i] == '1') || (s[i] == '3') || (s[i] == '5') || (s[i] == '7') || (s[i]=='9'))
+			count++;
+	}
+	cout << count <<endl;
+	return 0;
+}
